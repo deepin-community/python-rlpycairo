a597fe6206bd4eb9a4b05007e176e84808eec789
李成刚 <lichenggang@uniontech.com>
2023-11-03 16:13:26 +0800

    feat: update python-rlpycairo to 0.3.0

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/python-rlpycairo/issues
+# Bug-Submit: https://github.com/<user>/python-rlpycairo/issues/new
+# Changelog: https://github.com/<user>/python-rlpycairo/blob/master/CHANGES
+# Documentation: https://github.com/<user>/python-rlpycairo/wiki
+# Repository-Browse: https://github.com/<user>/python-rlpycairo
+# Repository: https://github.com/<user>/python-rlpycairo.git
